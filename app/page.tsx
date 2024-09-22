70e48decbb3144b8fb3f5bc798441c2abf827ec9
Jasson Caceres <jassoncaceres@Jassons-MBP.lan>
2024-09-22 09:18:03 -0400

    changing bgColor

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const data = await fetchICSData(CALENDARID as string);
 
   return (
-    <div className=" bg-gradient-to-t from-green-700 to-emerald-400  min-h-screen">
+    <div className=" bg-gradient-to-t from-red-700 to-red-400  min-h-screen">
       <div className=" flex-col gap-10 container mx-auto min-h-screen  flex justify-center items-center w-full  ">
         <h1 className="  text-4xl max-w-lg text-center  font-bold text-balance">
           Retrieving Calendar dates from <span>Google Calendar</span>
